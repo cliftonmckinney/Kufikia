067f8f68ec1f553c0caa73110f7944537534a4fb
Brad Montgomery <brad@bradmontgomery.net>
2013-02-16 10:31:45 -0600

    added a fabfile for deployment

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import env
+from fabric.operations import put
+
+
+REMOTE_PROJECT_DIR = '/home/web/kufikia.com'
+env.hosts = ['root@kufikia.com', ]
+
+
+def upload():
+    """Uploads the site content to the remote project directory."""
+    put(local_path="Public/*", remote_path=REMOTE_PROJECT_DIR)
+
+
+def deploy():
+    """Deploy the site."""
+    upload()
